1ba7c638a928933122b7e44638cad2d11669c5b4
Daniel <daniel.ullrich@student.fhws.de>
2021-03-22 12:09:47 +0100

    init

---
+++ task04/coding/sound/Sound/Sound/main.c
@@ -0,0 +1,18 @@
+/*
+ * Sound.c
+ *
+ * Created: 22.03.2021 12:08:45
+ * Author : Daniel
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
