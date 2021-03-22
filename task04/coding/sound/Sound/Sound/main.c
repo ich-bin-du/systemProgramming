b49104afa7bec4a4d8ab53f1a353ac00d5cabe0b
Daniel <daniel.ullrich@student.fhws.de>
2021-03-22 12:27:12 +0100

    first soundoutput

--- task04/coding/sound/Sound/Sound/main.c
+++ task04/coding/sound/Sound/Sound/main.c
@@ -5,15 +5,22 @@
  * Author : Daniel
  */ 
 
+
+#define F_CPU 8000000
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+
 int main(void)
 {
+	PORTD = 0xFF;
+	DDRD = 0xFF;
     /* Replace with your application code */
     while (1) 
     {
+		PORTD--;
+		_delay_ms(100);
     }
 }
-
