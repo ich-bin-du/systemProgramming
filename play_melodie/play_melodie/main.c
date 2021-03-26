d3580798fbdeadb932702e7d5c9f9da617776adf
Amthor, Dominik <dominik.amthor@student.fhws.de>
2021-03-26 18:07:37 +0100

    Ich geh mit meiner Laterne Melodie hinzugefügt

--- play_melodie/play_melodie/main.c
+++ play_melodie/play_melodie/main.c
@@ -15,11 +15,35 @@
 volatile uint16_t timer = 0;
 
 // Music notes 
-enum music_notes { C = 523, D = 587, E = 659, F = 698, G = 784, A = 880, B = 988, H = 988, c = 1047 };
+enum music_notes { C= 523, C2= 261, D= 587, D4= 293, D5= 587, E = 659, F = 698, F2 = 349, G = 784, G4=391, A = 880, A4= 440, B = 988, B4= 493, H = 988, c = 1047, Eb = 622, Eb2= 311, Bb = 466, Ab = 415 };
 
 // Melodie and Timing for "Alle meine Entchen"
 const uint16_t entchen_melodie[] = { C, D, E, F, G, G, A, A, A, A, G, A, A, A, A, G, F, F, F, F, E, E, G, G, G, G, C };
 const uint8_t entchen_timing[] = { 40, 40, 40, 40, 80, 80, 40, 40, 40, 40, 80, 40, 40, 40, 40, 80, 40, 40, 40, 40, 80, 80, 40, 40, 40, 40, 80 };
+
+// Melodie and Timing for "Ich geh mit meiner Laterne"
+const uint16_t laterne_melodie[] = {
+	D4, G4, G4, B4, G4, B4, D5, B4, G4, A4, A4, A4, A4, B4, A4, G4, 
+	B4, D5, B4, G4, B4, D5, B4, G4, G4, A4, A4, A4, A4, B4, A4, G4
+};
+const uint8_t laterne_timing[] = {
+	20, 40, 20, 20, 20, 20, 50, 40, 20, 20, 20, 20, 20, 20, 50, 20,
+	20, 40, 20, 40, 20, 40, 20, 40, 20, 20, 20, 20, 20, 20, 20, 50
+};
+
+// Melodie and Timing for "Game of Thrones"
+const uint16_t got_melodie[] = { 
+		G, C, Eb, F, G, Eb, F, D,
+		F, Bb, Eb, D, F, Bb, Eb, D, C,
+		Ab, F2, C2, F2,   G4, C2,
+		C2, D4, Eb2, F2, G4,
+		};
+const uint8_t got_timing[] = { 
+		30, 30, 40, 30, 30, 40, 60, 50,
+		40, 60, 40, 40, 40, 20, 70, 40,
+		30, 30, 30, 60,   60, 80,
+		30, 30, 40, 30, 60
+		};
 
 /**
  *	Initiate PD4 (OC1B) and PD5 (OC1A) as Output.
@@ -58,8 +82,8 @@ void set_signal( uint16_t frequency )
  */
 void set_delay( uint8_t delay )
 {
-	while( timer <= delay*6 );
 	timer = 0;
+	while( timer <= delay*10 );
 }
 
 /**
@@ -75,11 +99,35 @@ void play_melodie_entchen( uint8_t *length )
 }
 
 /**
+ *	Play melodie "GoT".
+ */
+void play_melodie_got( uint8_t *length )
+{
+	for( int i = 0; i < *length; i++ )
+	{
+		set_signal( got_melodie[i] );
+		set_delay( got_timing[i] );
+	}
+}
+
+/**
+ *	Play melodie "Laterne".
+ */
+void play_melodie_laterne( uint8_t *length )
+{
+	for( int i = 0; i < *length; i++ )
+	{
+		set_signal( laterne_melodie[i] );
+		set_delay( laterne_timing[i] );
+	}
+}
+
+/**
  *	Returns the length of the melodie array.
  */
-uint8_t get_melodie_length( )
+uint8_t get_melodie_length()
 {
-	return sizeof(entchen_melodie) / sizeof(entchen_melodie[0]);
+	return sizeof(laterne_melodie) / sizeof(laterne_melodie[0]);
 }
 
 
@@ -92,7 +140,9 @@ int main( void )
 	
     while ( 1 )
     {
-		play_melodie_entchen( &length );
+		//play_melodie_entchen( &length );
+		play_melodie_laterne( &length );
+		//play_melodie_got( &length );
     }
 }
 
