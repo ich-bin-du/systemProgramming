f304826f4cc489afae817f9ad96bcdcab61cc78a
Amthor, Dominik <dominik.amthor@student.fhws.de>
2021-04-04 12:00:52 +0200

    init commit spi_sd-card

---
+++ uebung06/spi_sd-card/spi_sd-card/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ * Created: 04.04.2021 11:58:52
+ * Author : Dominik Amthor
+ */ 
+
+#include <avr/io.h>
+
+
+int main( void )
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
