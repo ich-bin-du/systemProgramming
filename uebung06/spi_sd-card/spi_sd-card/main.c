1c0af864748df93c628cc3fb6fe7c66dac09849f
Amthor, Dominik <dominik.amthor@student.fhws.de>
2021-04-04 16:49:09 +0200

    refactoring main.c

--- uebung06/spi_sd-card/spi_sd-card/main.c
+++ uebung06/spi_sd-card/spi_sd-card/main.c
@@ -4,26 +4,28 @@
  * Created: 04.04.2021 11:58:52
  * Author : Dominik Amthor
  */ 
-
+#ifndef F_CPU
+	#define F_CPU 8000000UL
+#endif
 #include <avr/io.h>
+#include "usart.h"
+#include "spi.h"
+#include "sd_card.h"
 
-void init_spi_master()
+void init()
 {
-	DDRB = (1 << DDB5) | (1 << DDB7) | (1 << DDB4);	// Init MOSI, SS and SCK as Output and MISO as Input
-
-	SPCR = (1 << SPE) | (1 << MSTR);	// SPI Control Register: activate SPE (SPI enable) and set MSTR -> Master Mode (Master/Slave select)
-}
-
-void transmit_data_master( unsigned char data )
-{
-	SPDR = data;	// SPI Data Register: Write data byte to SPDR to start data transfer
-
-	while( !(SPSR & (1 << SPIF)) );	// when data transfer complete SPI Interrupt Flag is set in SPI Status Register
+	DDRA = 0xff;	// LED Output for debugging
+	PORTA = 0xff;	// LEDs active low
 }
 
 int main( void )
 {
-    /* Replace with your application code */
+	init_usart();
+
+	setup_stdio_stream();
+
+    init_spi_master();
+
     while (1) 
     {
     }
