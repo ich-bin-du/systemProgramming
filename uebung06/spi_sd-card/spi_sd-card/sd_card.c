914309fe6dfe11905807cc713ba0d6736fb5a4a5
Amthor, Dominik <dominik.amthor@student.fhws.de>
2021-04-04 17:19:08 +0200

    add method sd_holder_hardware_reset

--- uebung06/spi_sd-card/spi_sd-card/sd_card.c
+++ uebung06/spi_sd-card/spi_sd-card/sd_card.c
@@ -7,16 +7,39 @@
 
  #include <avr/io.h>
  #include <util/delay.h>
+ #include <stdio.h>
  #include "sd_card.h"
 
  uint8_t CMD0[] = { 0x40, 0x00, 0x00, 0x00, 0x00, 0x95 };
 
+
  /*
   *	Init SD-Card.
   */
- void init_sd( void )
- {
+void sd_init( void )
+{
 	printf( "Start SD-Card init" );
 
-	DDRB = (1 << EN1) | (1 << EN2);	// EN1= Reset SD-Card (low= running mode / high = reset); EN2= Power SD-Card Holder (low= off / high= on); Timing signal min. 50ms
+	DDRB = (1 << EN1) | (1 << EN2);	// EN1= Reset SD-Card; EN2= Power SD-Card Holder
+
+	_delay_ms( 250 );	// Chapter 6.4. Power Scheme -> sd-card vdd_min is reached within 250ms
+
+	sd_holder_hardware_reset();
+
+
  }
+
+ /*
+  *	EN1= Reset SD-Card (low= running mode / high = reset)
+  * EN2= Power SD-Card Holder (low= off / high= on)
+  * Timing signal min. 50ms
+  */
+void sd_holder_hardware_reset( void )
+{
+	PORTB |= (1 << EN1);
+	PORTB &= ~(1 << EN2);
+	_delay_ms( 100 );
+	PORTB &= ~(1 << EN1);
+	PORTB |= (1 << EN2);
+}
+
