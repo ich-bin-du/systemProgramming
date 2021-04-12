84240e4629556669a7644fe4eaaf2a3d55b444f7
Amthor, Dominik <dominik.amthor@student.fhws.de>
2021-04-12 11:01:05 +0200

    init commit ultraschall/main.c

---
+++ uebung11/ultraschall/ultraschall/main.c
@@ -0,0 +1,18 @@
+/*
+ * ultraschall.c
+ *
+ * Created: 12.04.2021 10:57:20
+ * Author : DAmth
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
