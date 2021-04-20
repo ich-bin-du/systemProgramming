0cfdacf97db82d91c8290b6a236f39714d309e44
Amthor, Dominik <d.amthor@t-online.de>
2021-04-20 17:28:40 +0200

    add additional pin assignments and failure status codes

--- uebung06/spi_sd-card/spi_sd-card/sd_card.h
+++ uebung06/spi_sd-card/spi_sd-card/sd_card.h
@@ -10,20 +10,26 @@
 #define SD_CARD_H_
 
 // Pins for SD-Card Holder
-#define	EN1		PB1			// Reset of SD-Card (low = running mode / high = reset)
-#define EN2		PB0			// Power-Up SD-Card Holder (low = off / high = on)
+#define	EN1		DDB0		// Reset of SD-Card (low = running mode / high = reset)
+#define EN2		DDB1		// Power-Up SD-Card Holder (low = off / high = on)
+#define CD		DDB2		// Card detection (high = no card / low = card detected)
+#define WP		DDB3		// Write Protection (high = SD protected / low = SD not protected)
+#define LED		DDA0		// Status LED
+				
 
 // Command Pins
 #define SD_R7_CHECK_PATTERN		0xaa
 
 // Response token state
-#define SD_INIT_COMPLETED	0x00
-#define SD_IDLE_STATE		0x01
-#define SD_ERASE_RESET		0x02
+#define SD_INIT_COMPLETED		0x00
+#define SD_IDLE_STATE			0x01
+#define SD_ERASE_RESET			0x02
 
 // Status message
 #define SD_ERROR	1
 #define SD_RUNNING	0
+#define WRITE_PROTECTION_FAIL	0x02
+#define CARD_DETECTION_FAIL		0x03
 
 
 uint8_t sd_init( void );
@@ -53,6 +59,10 @@ void sd_send_read_ocr_CMD58( uint8_t *result );
 uint8_t sd_read_single_block( uint8_t *buffer );
 
 uint8_t sd_write_single_block( uint8_t *data );
+
+uint8_t is_write_protected( void );
+
+uint8_t is_card_detected( void );
 
 void set_delay_ms( uint16_t delay );
 
